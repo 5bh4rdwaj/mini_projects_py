a714454227fb4bff505ebff64ac30503e5459487
Samvat Bhardwaj <100153846+5bh4rdwaj@users.noreply.github.com>
2022-07-17 01:59:40 +0530

    extensive loooping, elif statements.

--- guess_numbers.py
+++ guess_numbers.py
@@ -8,15 +8,43 @@ top_of_range = input("Type a number: ")
 #checking to see if input was a number greater than 0
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
+   
     if top_of_range <= 0:
-        print("Please enter a number greater than 0 next time.")
+        print('Please type a number larger than 0 next time.')
         quit()
 else:
-    print("Please enter a number next time.")
+    print('Please type a number next time.')
     quit()
 
 
-
 # variable to store a random number b/w the lower limit x and upper limit top_of_range (x,top_of_range)
-random_number = random.randint(-2,top_of_range)
-print(random_number)
+random_number = random.randint(0,top_of_range)
+guess = 0
+
+
+# main stuff
+while True:
+    guess +=1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it! The number generated was "+ str(random_number))
+        break
+    elif user_guess < random_number:
+            print("You were below the number!")
+    else:
+        print("You were above the number!")
+
+
+
+
+#to print how many guesses it took for the user to land at the answer
+print("You got it in",guess,"guesses")
+#the above line is replacement to: print("You got it in "+ str(guesses)+" guesses")
+
+# End of program
