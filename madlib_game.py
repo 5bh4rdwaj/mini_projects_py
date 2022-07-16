6f60d48dd95a72dc451a725f8007c5b1da0bfd53
Samvat Bhardwaj <100153846+5bh4rdwaj@users.noreply.github.com>
2022-07-17 01:09:55 +0530

    uploaded madlib game
    Simple concepts, very similar syntax to java, no brainer

---
+++ madlib_game.py
@@ -0,0 +1,7 @@
+#classic mad lib game on the rosenburg principle
+colour=input("Enter a colour ")
+plural_noun=input("Enter a plural noun ")
+celebrity= input("Enter a celebrity's name ")
+print("Roses are "+colour)
+print(plural_noun+" are blue")
+print("I love "+celebrity +" and so do you.")
