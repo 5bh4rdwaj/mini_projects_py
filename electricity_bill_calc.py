6ed9b9a0e87a01a88bed18dfdfb889f885c25f4e
5bh4rdwaj <samvatb@protonmail.com>
2024-02-08 23:34:53 +0530

    electricity bill calculator: file update 1

--- electricity_bill_calc.py
+++ electricity_bill_calc.py
@@ -5,7 +5,7 @@ name=""
 address=""
 units_consumed = 0
 bill_amount = 0
-solar_writeoff = False
+solar_writeoff = 0
 
 # Accepting address, name and units of electricity consumed
 name=input("Hello. Please enter your name: ")
@@ -16,13 +16,12 @@ units_consumed =int(input ("And how many units of electricity did you consume th
 def solar_test():
     check =input ("Does your household have a solar panel? (Yes/No) ").lower()
     if check == "yes":
-        return True
+        solar_writeoff = 10
     else:
-       return False
+       solar_writeoff = 0
 
 
 # Calculation
-solar= solar_test()
 if units_consumed >= 1 and units_consumed <5 :
     #first 4 units cost 11 rupees per unit
     bill_amount = (units_consumed * 11)
@@ -36,5 +35,7 @@ else:
     print ("error")
 
 # Printing
+solar_test()
+bill_amount = bill_amount - ((solar_writeoff/100) * bill_amount)
 print ("Hello, "+ name)
-print ("\n Your electricity bill for this month is " + str(bill_amount))
+print ("\nYour electricity bill for this month is " + str(bill_amount))
